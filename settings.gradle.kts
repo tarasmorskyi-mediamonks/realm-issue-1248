3561fad774aa341207a7b2bd8d0620d219565364
Clemente <clemente.tort@mongodb.com>
2023-02-23 16:52:49 +0100

    Fix pod config by copying Realm multiplatform demo setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,24 @@
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        // Only required for realm-kotlin snapshots
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        // Only required for realm-kotlin snapshots
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+}
+
 rootProject.name = "My_Application"
 include(":androidApp")
 include(":shared")
